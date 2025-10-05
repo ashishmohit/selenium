40e20aff2c661727d058595cd1d5c41cdb6281de
ashushmohit <ashishthakur5280@gmail.com>
2025-10-05 15:01:57 +0530

    ashish1

---
+++ src/main/java/Test904/Test9045/App.java
@@ -0,0 +1,13 @@
+package Test904.Test9045;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Test904/Test9045/Myfirstmaven.java
@@ -0,0 +1,19 @@
+package Test904.Test9045;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Myfirstmaven {
+
+	public static void main(String[] args) throws IOException {
+//		FileInputStream x = new FileInputStream ("F:/Ashish/eclipse-workspace/Test9045/src/config.properties");
+//		Properties prop = new Properties ();
+//		prop.load(x);
+		
+		System.out.println( System.getProperty("author"));
+
+	}
+
+}
